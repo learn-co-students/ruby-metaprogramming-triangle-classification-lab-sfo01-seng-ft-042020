79b0b918c793192025f5fcd9029fa918303de3d4
Don Mallory <dmm333@gmail.com>
2020-04-27 22:03:11 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,18 +6,18 @@ class Triangle
   end
 
   def kind
-    if sides[2] >= sides[1] + sides[0] || sides.any? {|side| side <= 0}
+    if sides[2] >= sides[1] + sides[0] || sides[0] <= 0
       begin
         raise TriangleError
       end
     else
-      [:scalene, nil,:isosceles, :equilateral][sides.count {|side| sides.count(side) > 1}]
+      [nil, :equilateral, :isosceles, :scalene][sides.uniq.size]
     end
   end
 
   class TriangleError < StandardError
     def message
-      "Invalid triangle."
+      "Fake triangle! Sad!"
     end
   end
 end
